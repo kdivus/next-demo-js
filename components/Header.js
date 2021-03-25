1b35d595941007f2d871aaa0577dbdb340f212a2
Shokre <kdivus@hotmail.com>
2021-03-25 01:28:54 +0100

    header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,13 @@
+import Head from 'next/head'
 import headerStyles from '../styles/Header.module.css'
 
 const Header = () => {
     return (
         <div>
-            
+            <h1 className={headerStyles.title}>
+                <span>WebDev</span>News
+            </h1>
+            <p className={headerStyles.description}>Lorem ipsum dolor sit amet</p>
         </div>
     )
 }
